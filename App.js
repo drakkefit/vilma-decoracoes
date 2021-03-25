840f1119704bfe7877867e5e637cbcd9c7c9eddc
drakkefit <adriano.barbosa100@gmail.com>
2021-03-25 14:23:47 -0300

    second calc

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
-export default class App extends Component <Props> {
+export default class App extends Component<Props> {
 
   constructor(props) {
     super(props)
-    this.state = { quantidade: 0, altura: 1.5, valorPago: 12, resultado: 0 }
+    this.state = {
+      quantidade: 0,
+      altura: 1.5,
+      valorPago: 12,
+      resultado: 0,
+      altCortina: 0,
+      valorCortina: 12,
+      altForro: 0,
+      valorForro: 10,
+      resultado2: 0,
+    }
+
     this.calcular = this.calcular.bind(this)
+
+    this.calcular2 = this.calcular2.bind(this)
 
   }
 
@@ -18,25 +31,34 @@ export default class App extends Component <Props> {
     this.setState(this.setState(s))
   }
 
+  calcular2() {
+    let total = this.altCortina * this.valorCortina + this.altForro * this.valorForro
+
+    let s2 = this.state
+    s2.resultado2 = total
+    this.setState(this.setState(s2))
+  }
+
   render() {
-      return (
-        <View style={styles.container}>
-          <View>
-            <Text style={styles.h1}>Vilma decorações</Text>
-            <TextInput placeholder="Digite a quantidade" placeholderTextColor={'#f072a9'} keyboardType="numeric" style={styles.input} onChangeText={(quantidade) => { this.setState({ quantidade }) }} />
-          </View>
-          <TouchableOpacity style={styles.button} onPress={this.calcular}><Text style={styles.buttonText}>Calcular</Text></TouchableOpacity>
-          <Text style={styles.resultado}>{`R$ ${this.state.resultado.toFixed(2)}`}</Text>
-          <View>
-            <TextInput placeholder="Digite a quantidade" placeholderTextColor={'#f072a9'} style={styles.input} />
-          </View>
-          <View>
-            <TouchableOpacity style={styles.button} onPress={() => { }}><Text style={styles.buttonText}>Calcular</Text></TouchableOpacity>
-            <Text style={styles.resultado}>resultado</Text>
-          </View>
+    return (
+      <View style={styles.container}>
+        <View>
+          <Text style={styles.h1}>Vilma decorações</Text>
+          <TextInput placeholder="Digite a quantidade" placeholderTextColor={'#f072a9'} keyboardType="numeric" style={styles.input} onChangeText={(quantidade) => { this.setState({ quantidade }) }} />
         </View>
-      );
-    
+        <TouchableOpacity style={styles.button} onPress={this.calcular}><Text style={styles.buttonText}>Calcular</Text></TouchableOpacity>
+        <Text style={styles.resultado}> {`R$ ${this.state.resultado.toFixed(2)}`} </Text>
+        <View style={styles.container2}>
+          <TextInput placeholder="Q. da altura cortina" placeholderTextColor={'#f072a9'} keyboardType="numeric" style={styles.input} onChangeText={(altCortina) => { this.setState({ altCortina }) }} />
+          <TextInput placeholder="Q. da altura forro" placeholderTextColor={'#f072a9'} keyboardType="numeric" style={styles.input} onChangeText={(altForro) => { this.setState({ altForro }) }} />
+        </View>
+        <View>
+          <TouchableOpacity style={styles.button} onPress={this.calcular2}><Text style={styles.buttonText}>Calcular</Text></TouchableOpacity>
+          <Text style={styles.resultado}>{`R$ ${this.state.resultado2.toFixed(2)}`}</Text>
+        </View>
+      </View>
+    );
+
   }
 }
 
@@ -64,7 +86,7 @@ const styles = StyleSheet.create({
     // borderColor: "#ffc2dc",
     borderRadius: 5,
     margin: 15,
-    height: 40,
+    height: 50,
     borderColor: '#7a42f4',
     borderWidth: 1
   },
